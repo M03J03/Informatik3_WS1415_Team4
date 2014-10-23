c24441e013af08bb07f3d94a014b79f9ff035b11
M03J03 <moejoe6606@gmail.com>
2014-10-23 21:20:25 +0200

    Parser Contract  #1

--- Client/Parser.cs
+++ Client/Parser.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics.Contracts;
 
 namespace DragonsAndRabbits.Client
 {
     class Parser
     {
+
+        String inputFromBuffer;
+
+
+
+
+
+        public Parser()
+        {
+
+
+        }
+
+
+
+        public void getInputFromBuffer(String inputFromBuffer)
+        {
+
+            Contract.Requires(inputFromBuffer.Length > 0);
+            Contract.Ensures(inputFromBuffer.Length > 0);
+            AnalyzeBuffer(inputFromBuffer);
+
+        }
+
+        private void AnalyzeBuffer(String bufferInput)
+        {
+
+
+
+        }
+
+
+
     }
 }
