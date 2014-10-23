8723d7fec1db99c314a4aaae3ab0cd40abef5cf4
kokhanyu <roman.kokhanyuk@student.reutlingen-university.de>
2014-10-23 22:58:01 +0200

    Updating

---
+++ UnitTestDragonsAndRabbits/UnitTestDragon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DragonsAndRabbits.Client;
+
+namespace UnitTestDragonsAndRabbits
+{
+    [TestClass]
+    class UnitTestDragon
+    {
+        [TestMethod]
+        public void TestMethodsDragon()
+        {
+            Dragon dragon = new Dragon();
+
+            Player p1 = new Player();
+            Player p2 = new Player();
+
+            dragon.stagHunt(p1, p2);
+
+            Assert.IsTrue(dragon.getFirstPlayer() != null);
+            Assert.IsTrue(dragon.getSecondPlayer() != null);
+            Assert.IsTrue(p1.getId() > 0);
+            Assert.IsTrue(p2.getName().Length > 3);
+        }
+    }
+}
--- UnitTestDragonsAndRabbits/UnitTestPlayer.cs
+++ UnitTestDragonsAndRabbits/UnitTestPlayer.cs
@@ -11,8 +11,10 @@ namespace UnitTestDragonsAndRabbits
         {
 
             Player p1 = new Player();
+            Player p2 = new Player();
 
             Assert.IsTrue(p1.getName != null);
+            Assert.IsTrue(p1.getName().lenght <= 3);
             Assert.IsTrue(p1.getId() > 0);
 
         }
