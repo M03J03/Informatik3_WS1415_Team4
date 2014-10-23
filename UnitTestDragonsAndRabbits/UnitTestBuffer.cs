9981dfc73fbe9b9ec223d0c49b44e12283da30a7
drfbot <zipfele@yahoo.com>
2014-10-23 15:14:12 +0200

    Buffertest #1

---
+++ UnitTestDragonsAndRabbits/UnitTestBuffer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDragonsAndRabbits
+{
+    [TestClass]
+    public class UnitTestBuffer
+    {
+        Buffer buffer;
+        String Message;
+
+        [TestMethod]
+
+        //this test ensures that there IS a buffer #1Test 
+        public void TestInitBuffer()
+        {
+            Buffer buffer = new Buffer();
+            Assert.IsTrue(buffer != null);
+
+        }
+
+        //this method ensures, that the transfer from Connector to Buffer works.
+        public void TestBufferAddMethod()
+        {
+            Assert.IsTrue(message!=null);
+            //init
+             this.buffer = new ArrayList();
+
+            
+                     
+        }
+
+        public void TestBufferGetMethod()
+        {
+
+        }
+
+        public void TestBufferFlushMethod()
+        {
+
+        }
+    }
+}
