e9985c4050a8f12d541641e4debea5da85ca98a2
kokhanyu <roman.kokhanyuk@student.reutlingen-university.de>
2014-10-23 16:59:13 +0200

    2.

--- Client/Player.cs
+++ Client/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics.Contracts;
 
 namespace DragonsAndRabbits.Client
 {
@@ -10,24 +11,43 @@ namespace DragonsAndRabbits.Client
         private int id;
         private String name;
         private int score;
-        private Boolean busy;
+        private Boolean busy = false;
         private int x, y;
+        private static int playerNumber = 0;
 
+        //Generates an object of Player.
+        public Player()
+        {
+            setID(++playerNumber);
+            setName(name);
+        }
+
+        //This method sets the id of the Player.
         private void setID(int id)
         {
+            Contract.Requires(id > 0);
+            Contract.Ensures(id > 0);
             this.id = id;
         }
 
-        public int getId() 
+        //This method gets the number of the Player.
+        public int getId()
         {
+            Contract.Requires(id > 0);
+            Contract.Ensures(id > 0);
             return id;
         }
 
+        //This method sets the name of the Player.
         private void setName(String name)
         {
+            Contract.Requires(name != null);
+            Contract.Requires(name.Length > 0);
+            Contract.Ensures(name != null && name.Length > 0);
             this.name = name;
         }
 
+        //This Method get the name of the Player
         public String getName()
         {
             return name;
@@ -43,26 +63,36 @@ namespace DragonsAndRabbits.Client
             return score;
         }
 
+        //This Method get the X-Coordinate
         private void setX(int x)
         {
+            Contract.Requires(x >= 0);
+            Contract.Ensures(x >= 0);
             this.x = x;
         }
 
+        //This Method get the X-Coordinate
         public int getX()
         {
+            Contract.Ensures(x >= 0);
             return x;
         }
 
+        //This Method set the Y-Coordinate
         private void setY(int y)
         {
+            Contract.Requires(y >= 0);
+            Contract.Ensures(y >= 0);
             this.y = y;
         }
 
+        //This Method get the Y-Coordinate
         public int getY()
         {
             return y;
         }
 
+        //This method starts the fight.
         public void figth()
         {
 
