a5556e868aa9ffb516c22269dad8570cafd0850c
kokhanyu <roman.kokhanyuk@student.reutlingen-university.de>
2014-10-23 22:20:27 +0200

    UnitTestPlayer

---
+++ UnitTestDragonsAndRabbits/UnitTestPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDragonsAndRabbits
+{
+    [TestClass]
+    public class UnitTestPlayer
+    {
+        [TestMethod]
+        public void TestInitPlayer()
+        {
+
+            Player p1 = new Player();
+
+            Assert.IsTrue(p1.getName != null);
+            Assert.IsTrue(p1.getId() > 0);
+
+        }
+    }
+}
