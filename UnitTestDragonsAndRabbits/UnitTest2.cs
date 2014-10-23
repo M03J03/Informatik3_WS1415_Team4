c1d6e31e0d3f5ddeee965b53e4c405e2f3bb6665
drfbot <zipfele@yahoo.com>
2014-10-23 14:50:36 +0200

    Unit test basis

---
+++ UnitTestDragonsAndRabbits/UnitTest2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDragonsAndRabbits
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+
+        //diese Klasse mal noch als Repo-Test
+        public void TestMethod1()
+        {
+        }
+    }
+}
