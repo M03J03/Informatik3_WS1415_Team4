dcb6f5f87cbdf220da878ea4ac4b89da3848a02b
drfbot <zipfele@yahoo.com>
2014-10-23 13:36:07 +0200

    Update buffer

--- Client/Buffer.cs
+++ Client/Buffer.cs
@@ -8,32 +8,36 @@ using System.Text;
 namespace DragonsAndRabbits.Client {
     class Buffer {
 
-        ArrayList bufferArrayList;
+        ArrayList bufferArrayList =new ArrayList();
     
 
 
-        Buffer(){
-           ArrayList bufferArrayList =new ArrayList();
-        }
-
+        //this method sends recieved packets from the server to the buffer.
         private void addMessage(String messagetoBuffer)
         {
             Contract.Assume(messagetoBuffer is String, "ERROR: WRONG TYPE - the addMessage Method got a wrong Object to save to Buffer!");
             Contract.Requires(bufferArrayList!=null);
 
             Contract.Ensures(bufferArrayList.Capacity > 0, "nothing sent to the bufferArrayList");
-            Contract.Ensures();
         }
 
+        //this method pulles out one messages from the buffer. Always the first element of the arraylist
         public void getMessage(){
+            Contract.Requires(bufferArrayList.Capacity!=0);
+            Contract.Requires(bufferArrayList.);
+            Contract.Ensures(return !=null);
+            Contract.Ensures(bufferArrayList.RemoveAt(0));
 
         }
 
+        //this method enables to delete one message. -> automatically called from getMessage()
         private void removeMessage(){
         
         }
 
-        public void flushBuffer(){
+        //this method provides the possibility to delete the whole buffer in case of errors.
+        public void flushBuffer()
+        {
 
         }
 
