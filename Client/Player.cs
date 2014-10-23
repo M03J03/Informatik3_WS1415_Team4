6fc53dd220ad9e51eca6a4524ada6c6ffa8363dc
kokhanyu <roman.kokhanyuk@student.reutlingen-university.de>
2014-10-23 12:30:37 +0200

    1.

--- Client/Player.cs
+++ Client/Player.cs
@@ -7,5 +7,73 @@ namespace DragonsAndRabbits.Client
 {
     class Player
     {
+        private int id;
+        private String name;
+        private int score;
+        private Boolean busy;
+        private int x, y;
+
+        private void setID(int id)
+        {
+            this.id = id;
+        }
+
+        public int getId() 
+        {
+            return id;
+        }
+
+        private void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        private void setScore(int score)
+        {
+            this.score = score;
+        }
+
+        public int getScore()
+        {
+            return score;
+        }
+
+        private void setX(int x)
+        {
+            this.x = x;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        private void setY(int y)
+        {
+            this.y = y;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public void figth()
+        {
+
+        }
+
+        public void rest()
+        {
+
+        }
+
+
+
     }
 }
