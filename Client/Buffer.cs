11f7862c6718d1f1221b8e0b92ffcbc14eb1b262
drfbot <zipfele@yahoo.com>
2014-10-23 12:57:42 +0200

    unit test folder established - buffer Contract first steps

--- Client/Buffer.cs
+++ Client/Buffer.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 
 namespace DragonsAndRabbits.Client {
     class Buffer {
-//Test über standrechner
+
+        ArrayList bufferArrayList;
+    
+
+
+        Buffer(){
+           ArrayList bufferArrayList =new ArrayList();
+        }
+
+        private void addMessage(String messagetoBuffer)
+        {
+            Contract.Assume(messagetoBuffer is String, "ERROR: WRONG TYPE - the addMessage Method got a wrong Object to save to Buffer!");
+            Contract.Requires(bufferArrayList!=null);
+
+            Contract.Ensures(bufferArrayList.Capacity > 0, "nothing sent to the bufferArrayList");
+            Contract.Ensures();
+        }
+
+        public void getMessage(){
+
+        }
+
+        private void removeMessage(){
+        
+        }
+
+        public void flushBuffer(){
+
+        }
+
     }
 }
