8df08e5d206dacff121f866616badebd93bf1521
drfbot <zipfele@yahoo.com>
2014-10-23 16:29:29 +0200

    ClientGUI - declaration

--- Client/ClientGUI.cs
+++ Client/ClientGUI.cs
@@ -9,6 +9,10 @@ namespace DragonsAndRabbits.Client
     class ClientGUI
     {
 
+        //because there is no agreement concerning the layout and the functionality of the ClientGUI yet, this class is empty.
+        //as long as there are no guidelines for functionality and therefore needed elements, this state will last.
+        //
+
 
         public static ClientGUI getGUI() {
             return this;
