c666a566b103c75d28a770c82d3bbf59650f8bb3
drfbot <zipfele@yahoo.com>
2014-10-24 09:37:12 +0200

    Buffer semi final, Buffer-Test complete

--- Client/Buffer.cs
+++ Client/Buffer.cs
@@ -8,13 +8,13 @@ using System.Text;
 namespace DragonsAndRabbits.Client {
     public class Buffer {
 
-        ArrayList bufferArrayList = new ArrayList();
-        int lockCount = 0;
-        int bufferSize = 0;
+        ArrayList bufferArrayList = null;
+        static int lockCount = 0;
+      
 
 
         //this method initializes an ArrayList only ONCE!
-        Buffer()
+      public  Buffer()
         {
             if (lockCount != 0)
             {
@@ -30,45 +30,66 @@ namespace DragonsAndRabbits.Client {
     
 
 
-        //this method sends recieved packets from the server to the buffer.
-        private void addMessage(String messagetoBuffer)
+        //this method sends recieved packets from the connector to the buffer.
+        public void addMessage(String messagetoBuffer)
         {
             Contract.Assume(messagetoBuffer is String, "ERROR: WRONG TYPE - the addMessage Method in class 'Buffer' got a wrong Input Type!");
             Contract.Requires(bufferArrayList!=null);
 
-            bufferSize++;
+            int count = bufferArrayList.Count;
 
+            bufferArrayList.Add(messagetoBuffer);
 
-            Contract.Ensures(bufferArrayList.Capacity > 0, "nothing sent to the bufferArrayList");
+            Contract.Ensures(bufferArrayList.Count > 0, "nothing sent to the bufferArrayList");
+            Contract.Ensures(bufferArrayList.Count > count);
         }
 
-        //this method pulles out one messages from the buffer. Always the first element of the arraylist
+        /// <summary>
+        /// this method pulls out one message (at index 0) from the buffer. Returnes that String
+        /// </summary>
+        /// <returns>String</returns>
         public string getMessage(){
-            Contract.Requires(bufferArrayList.Capacity!=0);
+            Contract.Requires(bufferArrayList.Count > 0);
+
+            int count = bufferArrayList.Count;
 
             String tmp = (String)bufferArrayList[0];
             removeMessage();
+    
+            Contract.Ensures(bufferArrayList.Count < count);
             return tmp;
-            
-
-           // Contract.ValueAtReturn<string> (bufferArrayList[0]);
-           // Contract.Ensures(bufferArrayList.RemoveAt(0));
-
         }
 
-        //this method enables to delete one message. -> automatically called from getMessage()
+        //this method enables to delete one message. -> called from getMessage()
         private void removeMessage(){
+            Contract.Requires(bufferArrayList.Count > 0);
+            int count = bufferArrayList.Count;
+           
             bufferArrayList.RemoveAt(0);
+
+            Contract.Ensures(bufferArrayList.Count < count);
+
         }
 
         //this method provides the possibility to delete the whole buffer in case of errors.
         public void flushBuffer()
         {
+            Contract.Requires(bufferArrayList != null);
+
             bufferArrayList.Clear();
 
-            Contract.Ensures(bufferArrayList.Capacity == 0);
+            Contract.Ensures(isEmpty());
 
         }
 
+        //method for comparing and for analytics
+        public bool isEmpty()
+        {
+            if (bufferArrayList.Count > 0)
+            {
+                return false;
+            }
+            else { return true; }
+        }
     }
 }
--- UnitTestDragonsAndRabbits/UnitTestBuffer.cs
+++ UnitTestDragonsAndRabbits/UnitTestBuffer.cs
@@ -1,44 +1,54 @@
 ﻿using System;
 using System.Collections;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DragonsAndRabbits.Client;
 
 namespace UnitTestDragonsAndRabbits
 {
     [TestClass]
     public class UnitTestBuffer
     {
-        Buffer buffer;
-        String Message;
+
 
         [TestMethod]
 
         //this test ensures that there IS a buffer #1Test 
         public void TestInitBuffer()
         {
-            Buffer buffer = new Buffer();
-            Assert.IsTrue(buffer != null);
+            DragonsAndRabbits.Client.Buffer buffer = new DragonsAndRabbits.Client.Buffer();
+            Assert.IsTrue(buffer != null && buffer is DragonsAndRabbits.Client.Buffer);
 
         }
 
-        //this method ensures, that the transfer from Connector to Buffer works.
+        //this method ensures, that the transfer from Connector to Buffer works...
         public void TestBufferAddMethod()
         {
-            Assert.IsTrue(message!=null);
-            //init
-             this.buffer = new ArrayList();
+            DragonsAndRabbits.Client.Buffer buffer = new DragonsAndRabbits.Client.Buffer();
+            buffer.addMessage("hello");
+            Assert.IsFalse(buffer.isEmpty());
 
-            
-                     
+
         }
 
         public void TestBufferGetMethod()
         {
+            DragonsAndRabbits.Client.Buffer buffer = new DragonsAndRabbits.Client.Buffer();
+            buffer.addMessage("hello");
+
+            String local = buffer.getMessage();
+            Assert.Equals(local, "hello");
+            Assert.IsTrue(buffer.isEmpty());
+           
 
         }
 
         public void TestBufferFlushMethod()
         {
+            DragonsAndRabbits.Client.Buffer buffer = new DragonsAndRabbits.Client.Buffer();
+            buffer.addMessage("hello");
 
+            buffer.flushBuffer();
+            Assert.IsTrue(buffer.isEmpty());
         }
     }
 }
