96933517d161f472718eb5949dc522b6e0a79860
M03J03 <moejoe6606@gmail.com>
2014-10-24 03:43:17 +0200

    expand Parser #5
    insert TestUnits Parser (still not complete)

--- Client/Parser.cs
+++ Client/Parser.cs
@@ -21,24 +21,27 @@ namespace DragonsAndRabbits.Client
         public Parser()
         {
 
-           
-            
-         /*   validPrompts[0] = "update";
-            validPrompts[1] = "delete";
-            validPrompts[2] = "map";
-            validPrompts[3] = "message";
-            validPrompts[4] = "answer";
-            validPrompts[5] = "result";
-            validPrompts[6] = "challenge";
-            validPrompts[7] = "player";
-            validPrompts[8] = "yourid";
-            validPrompts[9] = "time";
-            validPrompts[10] = "online";
-            validPrompts[11] = "entities";
-            validPrompts[12] = "players";
-            validPrompts[13] = "server";
-        */
-
+            validPrompts[0] = "upd";            //
+            validPrompts[1] = "del";            //
+            validPrompts[2] = "map";            //
+            validPrompts[3] = "mes";            //
+            validPrompts[4] = "result";         //
+            validPrompts[5] = "challenge";      //
+            validPrompts[6] = "players";         //
+            validPrompts[7] = "yourid";         //
+            validPrompts[8] = "time";           //
+            validPrompts[9] = "online";        //
+            validPrompts[10] = "ents";          //
+            validPrompts[11] = "player";       //
+            validPrompts[12] = "server";        //
+            validPrompts[13] = "";              //  
+            validPrompts[14] = "opponent";      //
+            validPrompts[15] = "dragon";        //
+            validPrompts[16] = "cell";          //
+            validPrompts[17] = "ok";            //
+            validPrompts[18] = "unknown";       //
+            validPrompts[19] = "no";            //
+            validPrompts[20] = "invalid";       //
 
 
         }
@@ -78,33 +81,181 @@ namespace DragonsAndRabbits.Client
                 checkMsg(stringArrayOfValidMsg);
         }
 
-        private void checkMsg(String [] msgs)
+        public void checkMsg(String [] msgs)
         {
-            Contract.Requires(msgs.Length > 0);
-            Contract.Ensures(msgs.Length > 0);
+            Contract.Requires(msgs.Length >= 0);
+            Contract.Ensures(msgs.Length >= 0);
 
             bool goOn = true;
             int Counter = 0;
-            for (int i = msgs.Length; i >= 0; i--)
+            for (int i = 0; i < msgs.Length; i++)
             {
-                    
-                // MSGS at Position[i] contains a Rule to resolve the receiver of the message;
-                // Send valid MSG to Receiver with validationOfMessage = true;
-                
 
+                // while there is no match && there are valid prompts left to compare with
+                while (goOn && (Counter < validPrompts.Length))
+                {
+                    if (msgs[i].Contains(validPrompts[Counter]))
+                    {
+                        goOn = false;
 
+                        // Call one method listed below and parse this MESSAGE to the corresponding Class
+                    }
+                    else
+                    {
+                        Counter++;
+                    }
 
+                } // end while
             }
-
-
         }
 
-        private void sendMsgToListener(String Msg, bool isValid){
+        public void sendMsgToListener(String Msg, bool isValid){
                Contract.Ensures(Msg.Length > 0);
                Contract.Ensures(isValid);
         }
 
-        private void sendMsgToSomeObject(String Msg, bool isValid)
+        //Message begin:upd
+        public void sendMsgToUpdater(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:del
+        public void sendMsgToBackend(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        //Message begin:map
+        public void sendMsgToMap(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        //Message begin:mes
+        public void sendMsgToMessenger(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        //Message begin:result
+        public void sendMsgToResulter(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        //Message begin:challenge
+        public void sendMsgToChallenger(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:players
+        public void sendMsgToPlayers(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:yourid
+        public void sendMsgToPlayer(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:time
+        public void sendMsgToBackend(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:online
+        public void sendMsgToBackend(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:ents
+        public void sendMsgToBackend(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:player
+        public void sendMsgToPlayer(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:server
+        public void sendMsgToUNKNOWN(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:<empty>
+        public void sendMsgToIGNORE(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:opponent
+        public void sendMsgToChallenger(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:dragon
+        public void sendMsgToDragon(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:cell
+        public void sendMsgToMap(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:ok
+        public void sendMsgToMessenger(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:unknown
+        public void sendMsgToUNKNOWN(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:no
+        public void sendMsgToMessenger(String Msg, bool isValid)
+        {
+            Contract.Ensures(Msg.Length > 0);
+            Contract.Ensures(isValid);
+        }
+
+        // Message begin:invalid
+        public void sendMsgToBackend(String Msg, bool isValid)
         {
             Contract.Ensures(Msg.Length > 0);
             Contract.Ensures(isValid);
--- UnitTestDragonsAndRabbits/UnitTestParser.cs
+++ UnitTestDragonsAndRabbits/UnitTestParser.cs
@@ -8,7 +8,7 @@ namespace UnitTestDragonsAndRabbits
     public class UnitTestParser
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodGetInputFromBuffer()
         {
             Parser p = new Parser();
             p.getInputFromBuffer("Something");
@@ -16,7 +16,7 @@ namespace UnitTestDragonsAndRabbits
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void TestMethodAnalyzeBuffer()
         {
             Parser p = new Parser();
             p.analyzeBuffer("begins: end:");
@@ -26,7 +26,7 @@ namespace UnitTestDragonsAndRabbits
         }
 
         [TestMethod]
-        public void TestMethod3()
+        public void TestMethodAnalyzeBuffer()
         {
             Parser p = new Parser();
             p.analyzeBuffer("Bullshit");
@@ -37,13 +37,168 @@ namespace UnitTestDragonsAndRabbits
 
 
         [TestMethod]
-        public void TestMethod4()
+        public void TestMethodCheckMsg()
         {
             Parser p = new Parser();
-            p.analyzeBuffer("Bullshit");
-            Assert.IsFalse(p.getInput().Equals("begins:"));
+            String[] testString = { "begin: begin:server ver:5 end:server", "begin: begin:server ver:5 end:server", "begin: begin:server ver:5 end:server" };
+            
+            Assert.IsTrue((p.checkMsg(testString).Length >= 0);
             Assert.IsFalse(p.getInput().Equals("ends:"));
 
         }
+
+         [TestMethod]
+        public void TestMethodSendMsgToListener()
+        {
+
+        }
+
+         //Message begin:upd
+        [TestMethod]
+        public void TestMethodSendMsgToUpdater()
+        {
+
+        }
+
+        // Message begin:del
+        [TestMethod]
+        public void TestMethodSendMsgToBackend()
+        {
+
+        }
+
+        //Message begin:map
+        [TestMethod]
+        public void TestMethodSendMsgToMap()
+        {
+
+        }
+
+        //Message begin:mes
+        [TestMethod]
+        public void TestMethodSendMsgToMessenger()
+        {
+            
+        }
+
+        //Message begin:result
+        [TestMethod]
+        public void TestMethodsendMsgToResulter()
+        {
+          
+        }
+
+        //Message begin:challenge
+        [TestMethod]
+        public void TestMethodSendMsgToChallenger()
+        {
+
+        }
+
+        // Message begin:players
+        [TestMethod]
+        public void TestMethodSendMsgToPlayers()
+        {
+
+        }
+
+        // Message begin:yourid
+        [TestMethod]
+        public void TestMethodSendMsgToPlayer()
+        {
+      
+        }
+
+        // Message begin:time
+        [TestMethod]
+        public void TestMethodSendMsgToBackend()
+        {
+
+        }
+
+        // Message begin:online
+        [TestMethod]
+        public void TestMethodSendMsgToBackend()
+        {
+
+        }
+
+        // Message begin:ents
+        [TestMethod]
+        public void TestMethodSendMsgToBackend()
+        {
+
+        }
+
+        // Message begin:player
+        [TestMethod]
+        public void TestMethodsendMsgToPlayer()
+        {
+   
+        }
+
+        // Message begin:server
+        [TestMethod]
+        public void TestMethodSendMsgToUNKNOWN()
+        {
+
+        }
+
+        // Message begin:<empty>
+        [TestMethod]
+        public void TestMethodSendMsgToIGNORE()
+        {
+
+        }
+
+        // Message begin:opponent
+        [TestMethod]
+        public void TestMethodSendMsgToChallenger()
+        {
+
+        }
+
+        // Message begin:dragon
+        [TestMethod]
+        public void TestMethodSendMsgToDragon()
+        {
+
+        }
+
+        // Message begin:cell
+        [TestMethod]
+        public void TestMethodSendMsgToMap()
+        {
+
+        }
+
+        // Message begin:ok
+        [TestMethod]
+        public void TestMethodSendMsgToMessenger()
+        {
+
+        }
+
+        // Message begin:unknown
+        [TestMethod]
+        public void TestMethodSendMsgToUNKNOWN()
+        {
+
+        }
+
+        // Message begin:no
+        [TestMethod]
+        public void TestMethodSendMsgToMessenger()
+        {
+
+        }
+
+        // Message begin:invalid
+        [TestMethod]
+        public void TestMethodsendMsgToBackend()
+        {
+
+        }
+
     }
 }
