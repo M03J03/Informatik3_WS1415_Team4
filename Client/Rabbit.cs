893e50ec1da8bea448cd804f2c18406cbd8521f5
anawak84 <therealkoko@web.de>
2014-10-27 00:29:25 +0100

    Wrote the methods for the class Rabbit

--- Client/Rabbit.cs
+++ Client/Rabbit.cs
@@ -22,39 +22,41 @@ namespace DragonsAndRabbits.Client {
 
         //This method sets the id of the rabbit.
         private void setID(int id) {
-            Contract.Requires(id > 0);
-            Contract.Ensures(id > 0);
-            this.id = id;                        
+            if (id <= 0)
+            {
+                throw new Exception("Please insert a number larger than 0!");
+            }
+            else
+            {
+                this.id = id;
+            }                                            
         }
 
         //This method gets the actual number of the rabbit.
-        public int getID() {
-            Contract.Requires(id > 0);
-            Contract.Ensures(id > 0);
+        public int getID() 
+        {
             return id;
         }
 
         //This method sets the name of the rabbit. The name consists of the name Rabbit and the actual instance number of the rabit
-        private void setName() {
-            Contract.Requires(id > 0);
-            Contract.Ensures(id > 0);
+        private void setName() 
+        {            
             string n = "Rabbit" + getID();
             this.name = n;
         }
 
         //This Method get the name of the rabbit
         public string getName() {
-            Contract.Requires(name != null);
-            Contract.Requires(name.Length > 0);
-            Contract.Ensures(name != null && name.Length > 0);
-            return name;
+            if (name == null || name.Length < 1)
+            {
+                throw new Exception("There is no name existing or the name length is smaller than 1!");
+            }
+
+            return name;            
         }
 
         //This method set the palyer1
-        private void setPlayer1(Player player) {
-            Contract.Requires(player != null);
-            Contract.Ensures(this.player1.Equals(player));
-            
+        private void setPlayer1(Player player) {                        
             if (player == null)
             {
                 throw new Exception("No Player!");
@@ -67,19 +69,21 @@ namespace DragonsAndRabbits.Client {
 
         //This method gets the Player1.
         public Player getPlayer1() {
-            Contract.Requires(player1 != null);
-            Contract.Ensures(player1 != null); // <-- What if player 1 is not set? In this case player1 is null! 
-            return player1;
+            if (this.player1 == null)
+            {
+                throw new Exception("Player 1 is null");
+            }
+            else
+            {
+                return player1;
+            }            
         }
 
         //Set the player2
-        private void setPlayer2(Player player) {
-            Contract.Requires(player != null);
-            Contract.Ensures(this.player2.Equals(player));
-
+        private void setPlayer2(Player player) {            
             if (player == null)
             {
-                throw new Exception("No Player!");
+                throw new Exception("Player 2 is null!");
             }
             else
             {
@@ -89,41 +93,69 @@ namespace DragonsAndRabbits.Client {
 
         //Get the player2
         public Player getPlayer2() {
-            Contract.Requires(player2 != null);
-            Contract.Ensures(player2 != null);  //<-- What if player 2 is not set? In this case player2 is null! 
-            return player2;
+            if (player2 == null)
+            {
+                throw new Exception("Player 2 is null!");
+            }
+            else
+            {
+                return player2;
+            }
         }
 
         //Sets the xCoordinate
         private void setXCoordinate(int i) {
-            Contract.Requires(i >= 0);
-            Contract.Ensures(i >= 0);
-            this.xCoordinate = i;
+            if (i < 0)
+            {
+                throw new Exception("The number of i is smaller than 0!");
+            }
+            else
+            {
+                this.xCoordinate = i;
+            }                        
         }
 
         //Get the xCoordinate
-        public int getXCoordinate() {
-            Contract.Ensures(xCoordinate >= 0);
+        public int getXCoordinate() {            
             return xCoordinate;
         }
 
         //Sets the xCoordinate
         private void setYCoordinate(int i) {
-            Contract.Requires(i >= 0);
-            Contract.Ensures(i >= 0);
-            this.yCoordinate = i;
+            if (i < 0)
+            {
+                throw new Exception("The number of i is smaller than 0!");
+            }
+            else
+            {
+                this.yCoordinate = i;
+            } 
         }
 
         //Gets the y-Coordinate
-        public int getYCoordinate() {
-            Contract.Ensures(xCoordinate >= 0);
+        public int getYCoordinate() {            
             return xCoordinate;
         }
 
 
         //This method starts the stagHuntGame. The method neds
         public void stagHunt(Player player1, Player player2) {
-          /*  Contract.Requires(player1 != null && player2 != null);
+            if (player1 == null || player2 == null)
+            {
+                throw new Exception("Player 1 or 2 is null!");
+            }
+            else
+            {
+                if (!(getPlayer1().isBusy()) && (!(getPlayer2().isBusy())))
+                {
+                    //bin zu müde zum coden ich mach den scheiß morgen
+                }
+                
+                setPlayer1(player1);
+                setPlayer2(player2);
+            }
+
+            Contract.Requires(player1 != null && player2 != null);
             Contract.Requires((getPlayer1().getXCoordinate() == this.getXCoordinate()) && (getPlayer1().getYCoordinate() == this.getYCoordinate()));
             Contract.Requires((getPlayer2().getXCoordinate() == this.getXCoordinate()) && (getPlayer2().getYCoordinate() == this.getYCoordinate()));
             Contract.Requires(player1.isBusy() && player2.isBusy());
@@ -131,9 +163,7 @@ namespace DragonsAndRabbits.Client {
             Contract.Ensures((getPlayer1().getXCoordinate() == this.getXCoordinate()) && (getPlayer1().getYCoordinate() == this.getYCoordinate()));
             Contract.Ensures((getPlayer2().getXCoordinate() == this.getXCoordinate()) && (getPlayer2().getYCoordinate() == this.getYCoordinate()));
             setPlayer1(player1);
-            setPlayer2(player2);
-
-           * */
+            setPlayer2(player2);           
         }
     }
 }
