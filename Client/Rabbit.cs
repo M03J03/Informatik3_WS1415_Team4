26f5c8a05b255a573874d22cc4b22c071b118ba9
anawak84 <therealkoko@web.de>
2014-10-29 10:04:04 +0100

    Fixing a compile error

--- Client/Rabbit.cs
+++ Client/Rabbit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics.Contracts;
-using DragonsAndRabbits.Exceptions
+using DragonsAndRabbits.Exceptions;
 
 namespace DragonsAndRabbits.Client {
     public class Rabbit {
