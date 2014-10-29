c4bd7ddba51d5a3766668fda62366ce9381e07a0
anawak84 <therealkoko@web.de>
2014-10-29 14:39:25 +0100

    Do same stuff again -.-

--- Client/Controler.cs
+++ Client/Controler.cs
@@ -4,64 +4,282 @@ using System.Linq;
 using System.Text;
 using DragonsAndRabbits.Client;
 using System.Diagnostics.Contracts;
+using DragonsAndRabbits.Exceptions;
 
-namespace DragonsAndRabbits.Client {
-    public class Controler {
+namespace DragonsAndRabbits.Client
+{
+    public class Controler
+    {
 
         private ClientGUI gui = null;
+        private Map map;
         private Player player = null;
         private string message;
 
-        //Constructor get a static v
-        public Controler(ClientGUI gui) {
-            this.gui = gui;
-        }
-
+        /// <summary>
+        /// Default-constructor
+        /// </summary>
         public Controler()
         {
-            // TODO: Complete member initialization
+
         }
 
-        //Gets the message
-        public string getMessage() {
+        /// <summary>
+        /// Creating an object with the commited parameter.
+        /// </summary>
+        /// <param name="gui"></param>
+        public Controler(ClientGUI gui, Map map)
+        {
+            setGUI(gui);
+            setMap(map);
+        }
+
+        /// <summary>
+        /// Sets the gui,
+        /// </summary>
+        /// <param name="gui"></param>
+        private void setGUI(ClientGUI gui)
+        {
+            if (gui == null)
+            {
+                throw new NullReferenceException("There is no GUI!");
+            }
+            else
+            {
+                this.gui = gui;
+            }
+        }
+
+        /// <summary>
+        /// gets the Client gui Object.
+        /// </summary>
+        /// <returns></returns>
+        public ClientGUI getGUI()
+        {
+            return gui;
+        }
+
+        /// <summary>
+        /// sets the map.
+        /// </summary>
+        /// <param name="map">the required parameter</param>
+        private void setMap(Map map)
+        {
+            if (map == null)
+            {
+                throw new NullReferenceException("There is no Map");
+            }
+            else
+            {
+                this.map = map;
+            }
+        }
+
+        /// <summary>
+        /// returns the map
+        /// </summary>
+        /// <returns>the map objects</returns>
+        private Map getMap()
+        {
+            return map;
+        }
+
+        /// <summary>
+        /// Returns the commited message.
+        /// </summary>
+        /// <returns>the commited message</returns>
+        public string getMessage()
+        {
             return message;
         }
 
-        //This method is responsible to let the player move left.
-        public void moveLeft() {
+        /// <summary>
+        /// sets the playder object
+        /// </summary>
+        /// <param name="player"></param>
+        private void setPlayer(Player player)
+        {
+            if (player == null)
+            {
+                throw new NullReferenceException("There is no player");
+            }
+            else
+            {
+                this.player = player;
+            }
+        }
+
+        /// <summary>
+        /// Returns the player object.
+        /// </summary>
+        /// <returns></returns>
+        public Player getPlayer()
+        {
+            return player;
+        }
+
+        /// <summary>
+        /// This method move the Player one step left
+        /// </summary>
+        public void moveLeft()
+        {
+            try
+            {
+                if (getPlayer() != null)
+                {
+                    //I need a method like this. This method should go with the player one step left.
+                    getMap().moveLeft();
+                }
+                else
+                {
+                    throw new NoPlayerException("There is no player!");
+                }
+            }
+            catch (NoPlayerException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
             Contract.Ensures(player != null);
         }
 
-        //This method is responsible to let the player move rigth
-        public void moveRigth() {
+        /// <summary>
+        /// This method move the Player one step rigth
+        /// </summary>
+        public void moveRigth()
+        {
+            try
+            {
+                if (getPlayer() != null)
+                {
+                    //I need a method like this. Thismethod should go withthe player one step rigth.
+                    getMap().moveRigth();
+                }
+                else
+                {
+                    throw new NoPlayerException("There is no player!");
+                }
+            }
+            catch (NoPlayerException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
             Contract.Ensures(player != null);
         }
 
-        // this method is responsible to let the player move up
-        public void moveUp() {
+        /// <summary>
+        /// This method move the player one step up.
+        /// </summary>
+        public void moveUp()
+        {
+            try
+            {
+                if (getPlayer() != null)
+                {
+                    //I need a method like this. This method should go with the player one step up.
+                    getMap().moveUp();
+                }
+                else
+                {
+                    throw new NoPlayerException("There is no player!");
+                }
+            }
+            catch (NoPlayerException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
             Contract.Ensures(player != null);
         }
 
-        // this method is responsible to let the player move down
-        public void moveDown() {
+        /// <summary>
+        /// this method is responsible to let the player move down
+        /// </summary>
+        public void moveDown()
+        {
+            try
+            {
+                if (getPlayer() != null)
+                {
+                    //I need a method like this. This method should go with the player one step down.
+                    getMap().moveDown();
+                }
+                else
+                {
+                    throw new NoPlayerException("There is no player!");
+                }
+            }
+            catch (NoPlayerException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
             Contract.Ensures(player != null);
         }
 
-        //this method let attack a player
-        public void attack() {
+        /// <summary>
+        /// this method let attack a player
+        /// </summary>
+        public void attack()
+        {
+            try
+            {
+                if (getPlayer() != null)
+                {
+                    //I need a method like this. This method should the player attack.
+                    getMap().attack();
+                }
+                else
+                {
+                    throw new NoPlayerException("There is no player!");
+                }
+            }
+            catch (NoPlayerException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
             Contract.Ensures(player != null);
         }
 
-        // this Method sends the written message in the chat box to the server.
-        public void sendMessage(string message) {
+        /// <summary>
+        ///  This Method sends the written message in the chat box to the server.
+        /// </summary>
+        /// <param name="message"></param>
+        public void sendMessage(string message)
+        {
             Contract.Requires(message.Length > 0);
+
+            try
+            {
+                if (message != null && message.Length > 0)
+                {
+                    this.message = message;
+                    //do something
+                }
+                else
+                {
+                    throw new NoMessageException("There is no Message!");
+                }
+            }
+            catch (NoMessageException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+
             Contract.Ensures(message.Length > 0);
-            this.message = message;
         }
 
-        // this method gets the written messages from other player to show the on the chatbox. This methos is running in a Thread.
-        public void revieveMessage() {
-        
+        /// <summary>
+        /// this method gets the written messages from other player to show the on the chatbox. This methos is running in a Thread. The method is also connected to the parser
+        /// </summary>
+        /// <param name="p"></param>
+        public void revieveMessage(Parser p)
+        {
+            if (p == null)
+            {
+                throw new NullReferenceException("There is no Parser!");
+            }
+            else
+            {
+                //i have no idea
+            }
         }
     }
 }
