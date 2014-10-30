f90e1f0b08f0453ac1b652f913049b4d11e9d812
anawak84 <therealkoko@web.de>
2014-10-30 12:54:14 +0100

    Creating the Exception class NoValidMessageException

---
+++ Exceptions/NoValidMessageExcecption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DragonsAndRabbits.Exceptions
+{
+    [Serializable]
+    public class  NoValidMessageExcecption : System.Exception
+    {
+        /// <summary>
+        /// Generates an Exception Object.
+        /// </summary>
+        public  NoValidMessageExcecption()
+        {
+        }
+
+        /// <summary>
+        /// Generates an Exception Object withe the commited parameter
+        /// </summary>
+        /// <param name="message"></param>
+        public  NoValidMessageExcecption(String message)
+            : base(message)
+        {
+        }
+    }
+}
