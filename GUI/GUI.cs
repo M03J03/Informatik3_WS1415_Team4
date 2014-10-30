9b3ba1ced989abf12c82f222edd534c440c210c8
M03J03 <moejoe6606@gmail.com>
2014-10-30 22:09:51 +0100

    Still trying to fix Project

--- GUI/GUI.cs
+++ GUI/GUI.cs
@@ -16,35 +16,35 @@ namespace DragonsAndRabbits.GUI
             InitializeComponent();
         }
 
-    /// <summary>
-    /// this method displays the players ID in the GUI.
-    /// </summary>
-    /// <param name="id"></param>
-    public void yourId(int id)
-    {
-        this.playerIdLabel.Text = (String)("your id: #" + id);
-    }
+        /// <summary>
+        /// this method displays the players ID in the GUI.
+        /// </summary>
+        /// <param name="id"></param>
+        public void yourId(int id)
+        {
+            this.playerIdLabel.Text = (String)("your id: #" + id);
+        }
 
-    /// <summary>
-    /// this method shows up the current time in the GUI.
-    /// </summary>
-    /// <param name="time"></param>
-    public void time(long time)
-    {
-        //if necessary, a calculation from seconds to the format HH:MM:SS can be inserted here
-        this.timeLabel.Text = "current time:" + time;
-    }
+        /// <summary>
+        /// this method shows up the current time in the GUI.
+        /// </summary>
+        /// <param name="time"></param>
+        public void time(long time)
+        {
+            //if necessary, a calculation from seconds to the format HH:MM:SS can be inserted here
+            this.timeLabel.Text = "current time:" + time;
+        }
 
 
         /// <summary>
         /// this method displays the number of players online in the GUI.
         /// </summary>
         /// <param name="iOnline"></param>
-    public void oninePlayers(int iOnline)
-    {
-        this.onlinePlayersLabel.Text = "players online: " + iOnline;
-    }
-     
+        public void oninePlayers(int iOnline)
+        {
+            this.onlinePlayersLabel.Text = "players online: " + iOnline;
+        }
+
 
 
 
