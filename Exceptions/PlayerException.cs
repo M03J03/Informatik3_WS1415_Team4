f0ad0a67149273cefa82e0dba7bbf91eb73344f0
drfbot <zipfele@yahoo.com>
2014-10-30 16:10:48 +0100

    player exception extended

---
+++ Exceptions/PlayerException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DragonsAndRabbits.Exceptions
+{
+    [Serializable]
+    public class PlayerException : System.Exception
+    {
+        /// <summary>
+        /// Generates an Exception Object.
+        /// </summary>
+        public PlayerException()
+        {
+        }
+
+        /// <summary>
+        /// Generates an Exception Object withe the commited parameter
+        /// </summary>
+        /// <param name="message"></param>
+        public PlayerException(String message)
+            : base(message)
+        {
+        }
+    }
+}
