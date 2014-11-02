528d3ae2c10898e40508294ba6a966eb2f4fd068
drfbot <zipfele@yahoo.com>
2014-11-02 17:39:24 +0100

    Manager restructured - params redefined

--- Client/Manager.cs
+++ Client/Manager.cs
@@ -11,10 +11,12 @@ namespace DragonsAndRabbits.Client
     class Manager
     {
         private static Manager instance = null;
+        //only for test reasons vv
         private Dragon dragon = null;
         private Player player = null;
         private Rabbit rabbit = null;
         private Parser parser = null;
+         //only for test reasons ^^
         private GUI.GUI gui = null;
         private List<Dragon> listDragon = null;
         private List<Player> listPlayer = null;
@@ -23,7 +25,7 @@ namespace DragonsAndRabbits.Client
 
 
         /// <summary>
-        /// default-constructor. Generates one Instance of Manager. (Singleton)
+        /// private default-constructor. Generates one Instance of Manager. (Singleton)
         /// </summary>
         private Manager()
         {
@@ -58,70 +60,147 @@ namespace DragonsAndRabbits.Client
 
         //***************************CONTROL*MANAGEMENT********************
 
-
-        public void opponentInfo(String type, String info)
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="pID"></param>
+        /// <param name="decission"></param>
+        /// <param name="points"></param>
+        /// <param name="total"></param>
+        public void opponentInfo(int pID, String decission, int points, int total)
         {
 
         }
 
-        public void challengeInfo(String type, String info)
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="challengeID"></param>
+        /// <param name="type"></param>
+        /// <param name="accepted"></param>
+        public void challengeInfo(int challengeID, String type, bool accepted)
         {
 
         }
 
-        public void dragonInfo(String type, String info)
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="type"></param>
+        /// <param name="busy"></param>
+        /// <param name="decission"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void dragonInfo(int id, String type, bool busy, String decission, int x, int y)
         {
 
         }
 
-        public void playerInfo(String type, String info)
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="type"></param>
+        /// <param name="busy"></param>
+        /// <param name="name"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="points"></param>
+        public void playerInfo(int id, String type, bool busy, String name, int x, int y, int points)
         {
 
         }
 
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="o"></param>
         public void entitysInfo(Object o)
         {
             if (o is Dragon)
             {
             }
-            else if(o is Player){
+            else if (o is Player)
+            {
             }
-            else{
+            else
+            {
                 throw new NotSupportedException("the entity given to the manager is invalid.");
             }
         }
 
-        public void mapCellInfo(String type, String info)
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <param name="props"></param>
+        public void mapCellInfo(int row, int col, List<String> props)
         {
-           
+            if (props.Count < (row * col))
+            {
+                throw new ManagerInputException("mapcell information is corrupt! ");
+            }
+            else
+            {
+
+            }
         }
 
-        public void mapInfo(String type, String info)
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="breite"></param>
+        /// <param name="höhe"></param>
+        /// <param name="mcList"></param>
+        public void mapInfo(int breite, int höhe, List<MapCell> mcList)
         {
 
         }
 
-        public void Msg(String type, String info)
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="sourceid"></param>
+        /// <param name="src"></param>
+        /// <param name="text"></param>
+        public void Msg(int sourceid, String src, String text)
         {
 
         }
 
-        public void update(String type, String info)
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="p"></param>
+        /// <param name="mc"></param>
+        public void update(Dragon d, Player p, MapCell mc)
         {
 
         }
 
-        public void deleteInfo(String type, String info)
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="p"></param>
+        public void deleteInfo(Dragon d, Player p)
         {
 
         }
 
+        /// <summary>
+        /// this method recieves information from the parser and delegates it to the concerning backend-methods.
+        /// </summary>
+        /// <param name="ans"></param>
+        /// <param name="info"></param>
         public void anwerInfo(String ans, String info)
         {
-            
+
         }
 
-        
 
 
 
