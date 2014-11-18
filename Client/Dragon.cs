dfeac04e8f42a31e39ae7b8c035d4e8c57678793
kalasE <eugen.kalas@student.Reutlingen-university.de>
2014-11-18 17:35:12 +0100

    Same Updates for Class Dragon

--- Client/Dragon.cs
+++ Client/Dragon.cs
@@ -10,53 +10,14 @@ namespace DragonsAndRabbits.Client
     public class Dragon : Piece
     {
 
-
         /// <summary>
         ///  Generates an object of Dragon.
         /// </summary>
-        public Dragon(int id, String name, int xCoordinate, int yCoordinate)
+        public Dragon(int id, String name, int xCoordinate, int yCoordinate):base(id, name, xCoordinate, yCoordinate)
         {
-            setID(id);
-            setName(name);
-            setXCoordinate(xCoordinate);
-            setYCoordinate(yCoordinate);
+    
         }
 
-
-
-        /// <summary>
-        /// This Methos to update attribute of the Object Dragon
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="busy"></param>
-        /// <param name="xCoordinate"></param>
-        /// <param name="yCoordinate"></param>
-        /// <returns></returns>
-        public void update(int id, Boolean busy, int xCoordinate, int yCoordinate)
-        {
-            if (getID() == id)
-            {
-                setBusy(busy);
-                setXCoordinate(xCoordinate);
-                setYCoordinate(yCoordinate);
-            }
-
-        }
-
-        /// <summary>
-        /// This Methos to update attribute of the Object Dragon
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="busy"></param>
-        public void update(int id, Boolean busy)
-        {
-            if (getID() == id)
-            {
-                setBusy(busy);
-
-            }
-
-        }
 
     }
 }
