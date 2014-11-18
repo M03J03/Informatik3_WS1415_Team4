17fe14856e5a958420702ee82f811cba070fa18c
kalasE <eugen.kalas@student.Reutlingen-university.de>
2014-11-18 17:48:35 +0100

    Same Updates for Class Player

--- Client/Player.cs
+++ Client/Player.cs
@@ -7,69 +7,20 @@ using DragonsAndRabbits.Exceptions;
 
 namespace DragonsAndRabbits.Client
 {
-    public class Player
+    public class Player:Piece
     {
-        private int id;
-        private String name;
+
         private int score;
-        private Boolean busy = false;
-        private int xCoordinate;
-        private int yCoordinate;
-        private static int playerNumber = 0;
+
 
         /// <summary>
         /// Generates an object of Player
         /// </summary>
-        public Player()
+        public Player(int id,String name,int xCoordinate,int yCoordinate):base(id,name,xCoordinate,yCoordinate)
         {
-            setID(++playerNumber);
-            setName(name);
+
         }
 
-        /// <summary>
-        /// This method sets the id of the Player
-        /// </summary>
-        /// <param name="id"></param>
-        private void setID(int id)
-        {
-            Contract.Requires(id > 0);
-            Contract.Ensures(id > 0);
-            this.id = id;
-        }
-
-        /// <summary>
-        /// This method gets the number of the Player
-        /// </summary>
-        /// <returns>id</returns>
-        public int getId()
-        {
-            Contract.Requires(id > 0);
-            Contract.Ensures(id > 0);
-            return id;
-        }
-
-        /// <summary>
-        /// This method sets the name of the Player
-        /// </summary>
-        /// <param name="name"></param>
-        private void setName(String name)
-        {
-            Contract.Requires(name != null);
-            Contract.Requires(name.Length > 0);
-            if (name == null)
-                throw new NullReferenceException("The field <Name> must not be empty!");
-            Contract.Ensures(name != null && name.Length > 0);
-            this.name = name;
-        }
-
-        /// <summary>
-        /// This Method get the name of the Player
-        /// </summary>
-        /// <returns>name of the Player</returns>
-        public String getName()
-        {
-            return name;
-        }
 
         /// <summary>
         /// This Method set the Score
@@ -89,64 +40,6 @@ namespace DragonsAndRabbits.Client
             return score;
         }
 
-        /// <summary>
-        /// This Method set the x-Coordinate
-        /// </summary>
-        /// <param name="i"></param>
-        private void setXCoordinate(int i)
-        {
-            Contract.Requires(i >= 0);
-            Contract.Ensures(i >= 0);
-            this.xCoordinate = i;
-        }
-
-        /// <summary>
-        /// This Method get the x-Coordinate
-        /// </summary>
-        /// <returns>xCoordinate</returns>
-        public int getXCoordinate()
-        {
-            Contract.Ensures(xCoordinate >= 0);
-            return xCoordinate;
-        }
-
-        /// <summary>
-        /// This Method set the y-Coordinate
-        /// </summary>
-        /// <param name="i"></param>
-        private void setYCoordinate(int i)
-        {
-            Contract.Requires(i >= 0);
-            Contract.Ensures(i >= 0);
-            this.yCoordinate = i;
-        }
-
-        /// <summary>
-        /// This Method get the y-Coordinate
-        /// </summary>
-        /// <returns>y-Coordinate</returns>
-        public int getYCoordinate()
-        {
-            Contract.Ensures(xCoordinate >= 0);
-            return xCoordinate;
-        }
-
-        /// <summary>
-        /// Method to set actual status of the Player
-        /// </summary>
-        private void setBusy(Boolean busy)
-        {
-            this.busy = busy;
-        }
-
-        /// <summary>
-        /// Method to get actual status of the Player
-        /// </summary>
-        /// <returns>Is the Player busy or not busy</returns>
-        public Boolean isBusy()
-        {
-            return busy;
-        }
 
         /// <summary>
         /// This method starts the fight
@@ -162,23 +55,20 @@ namespace DragonsAndRabbits.Client
         }
 
         /// <summary>
-        /// this method to update attribyte of the object Player
+        /// this method to update Score of the object Player
         /// </summary>
         /// <param name="id"></param>
         /// <param name="bysu"></param>
         /// <param name="yCoordinate"></param>
         /// <param name="xCoordinate"></param>
         /// <returns></returns>
-        public Boolean update(int id, Boolean bysu, int yCoordinate, int xCoordinate)
+        public void update(int id, int score)
         {
-            if (this.id == id)
+            if (getID() == id)
             {
-                this.setBusy(busy);
-                this.setYCoordinate(yCoordinate);
-                this.setXCoordinate(xCoordinate);
-                return true;
+                this.setScore(score);
+
             }
-            return false;
         }
 
         /// <summary>
