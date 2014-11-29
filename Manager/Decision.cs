725926a1c59867a6a2b4b0858b811eed8c7f0b1a
anawak84 <therealkoko@web.de>
2014-11-29 15:01:23 +0100

    Generating class player. Reusing the enumeration Decision.

---
+++ Manager/Decision.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DragonsAndRabbits.Manager
+{
+    enum Decision
+    {
+        UNDEFINED,STAG,BUNNY,FIGTH,REST,SWORD,MAGIC,ALCHEMY
+    }
+}
---
+++ Manager/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DragonsAndRabbits.Manager
+{
+    class Player : Entity
+    {
+        private int points = 0;
+        //private Decission decision;
+
+
+        protected Player(int id, String name, bool busy, int row, int column, int points)
+            : base(id, name, busy, row, column)
+        { 
+            setPoints(points);
+            setDecision(Decision.UNDEFINED);
+        }
+
+    }
+}
