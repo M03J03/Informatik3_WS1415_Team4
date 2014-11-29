3eb9a1e5ba7bbb9b3a3cf766201e7d9fd4997bd9
anawak84 <therealkoko@web.de>
2014-11-29 15:36:27 +0100

    Redisigned Dragon object. Still not finished

---
+++ Manager/Dragon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DragonsAndRabbits.Manager
+{
+    class Dragon : Entity
+    {
+        private Dragon dragon;
+
+        /// <summary>
+        /// Generates an dragon object
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="busy"></param>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        protected Dragon(int id, String name, bool busy, int row, int column)
+            : base(id, name, busy, row,column)
+        {
+            setDragon(this);
+        }
+
+        /// <summary>
+        /// Sets the dragon object
+        /// </summary>
+        /// <param name="dragon"></param>
+        private void setDragon(Dragon dragon)
+        {
+            this.dragon = dragon;
+        }
+
+        /// <summary>
+        /// returnsthe dragon.
+        /// </summary>
+        /// <returns></returns>
+        protected Dragon getDragon()
+        {
+            return dragon;
+        }
+    }
+}
