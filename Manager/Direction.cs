e32923039252f9082ba8fb21dffb548f5301429f
drfbot <zipfele@yahoo.com>
2014-12-01 09:49:00 +0100

    enumeration direction and enumeration property added in Manager

---
+++ Manager/Direction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DragonsAndRabbits.Manager
+{
+    /// <summary>
+    /// enumeration of the directions 
+    /// </summary>
+    internal enum Direction {up, down, left, right};
+  
+}
---
+++ Manager/Property.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DragonsAndRabbits.Manager
+{
+    //PROPERTY: "WALKABLE"|"WALL"|"FOREST"|"WATER"|"HUNTABLE"
+ 
+    /// <summary>
+    /// enumeration of the properties of the tile
+    /// </summary>
+    internal enum Properties {walkable, wall, forest, water, huntable};
+}
